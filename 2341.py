32678eb8b92bae83240b6be316d4662ec09a5287
shibil-thora <shibilallakkat123@gmail.com>
2024-01-25 09:46:40 +0530

    the first commit

---
+++ 2341.py
@@ -0,0 +1,14 @@
+n = 34
+ 
+is_div = True
+
+for x in range(3,n+1):
+    if x % 2 == 1:
+        if n % x == 0:
+            is_div == False
+            break
+
+
+print(is_div)
+
+# ithil aadyam varunna For Loopil normal aaki declare cheyth. Eppozhaano divisibility varunnath appo
